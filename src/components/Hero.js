e241b4f50ee8b75d4a1ba9bccc7fbb1de8866ee9
HassanFouaad <mrhassanfouad1@gmail.com>
2020-08-28 03:08:21 +0200

    Landing Page and Hero Component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,34 @@
-import React from 'react'
-
-export default function Hero() {
-    return (
-        <div>
-            hello from Hero
-        </div>
-    )
+import React from "react";
+import styled from "styled-components";
+import mainBCG from "../images/mainBcg.jpeg";
+export default function Hero({ img, title, max, children }) {
+  return (
+    <HeroWrapper max={max} img={img}>
+      <div class="banner">
+        <h1 className="title">{title}</h1> {children}
+      </div>
+    </HeroWrapper>
+  );
 }
+
+const HeroWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: ${(props) => (props.max ? "100vh" : "60vh")};
+  color: var(--mainWhite);
+  background: linear-gradient(var(--primaryRGBA), var(--primaryRGBA)),
+    url(${(props) => props.img}) center/cover no-repeat;
+  .title {
+    padding-top: "2rem";
+    font-size: 3.5rem;
+    text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.3);
+    text-transform: uppercase;
+    letter-spacing: var(--mainSpacing);
+  }
+`;
+
+Hero.defaultProps = {
+  img: mainBCG,
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,12 @@
-import React from "react";
-import { ProductConsumer } from "../context/context";
-
+import React, { Fragment } from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 export default function HomePage() {
-  return <ProductConsumer></ProductConsumer>;
+  return (
+    <Fragment>
+      <Hero title="awesome gadgets" max="true">
+        <Link to="/products">Our Products {";)"}</Link>
+      </Hero>
+    </Fragment>
+  );
 }
